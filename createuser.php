0b1201a56b1c686de19f801ae2ea8cf182fe81dd
ravichandra2k01 <109383673+ravichandra2k01@users.noreply.github.com>
2023-03-09 10:17:08 +0530

    Update createuser.php

--- createuser.php
+++ createuser.php
@@ -72,7 +72,7 @@
   </div>
 </div>
 <footer class="text-center mt-5 py-2">
-            <p>&copy 2021. Created by <b>NAGABHUSHAN K M</b> <br>For The Sparks Foundation Internship Task</p>
+            <p>&copy 2021. Created by <b>Ravichandra</b> <br>For The Sparks Foundation Internship Task</p>
 </footer>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
